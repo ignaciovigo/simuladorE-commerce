4c73cb932f0456a4b0aa010ec09b171d1697e27f
ievigo <ignaciovigoe@gmail.com>
2022-09-27 23:12:30 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,15 @@
+alert('¡Bienvenido la verduleria!');
+let fruta = '';
+do{
+    fruta = prompt(`
+    1.PAPA     p.unitario -> 10$
+    2.CEBOLLA     p.unitario -> 20$
+    3.NARANJA     p.unitario -> 15$
+    4.MANZANA     p.unitario -> 10$
+    5.MANDARINA   p.unitairo -> 15$
+    6.TOMATE      p.unitairo -> 10$
+    7.FRUTILLA    p.unitario -> 8$
+    8.LECHUGA     p.unitario -> 30$
+    \nIngrese el nombre correspondiente de la fruta/verdura que desea comprar : `);
+}while(nombre != "s")
+
