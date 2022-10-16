b7e5f688c1acedc63a02e817004ed8158d1a6f55
ievigo <ignaciovigoe@gmail.com>
2022-10-16 00:07:08 -0300

    fix: nombre del archivo Carrito.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 import { comidas } from "./productos.js";
-import { Carrito } from "./carrito.js";
+import { Carrito } from "./Carrito.js";
 
 function pedirCantidad() {
   // pide un numero asegurandose que sea tipo number y lo retorna.
